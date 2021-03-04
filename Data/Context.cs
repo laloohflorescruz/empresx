14ce4afbfc6556be5f4fe343919e157c992c9827
Lalo Flores <eflores@crudcreativo.com>
2021-03-04 17:06:37 -0400

    DbContext

---
+++ Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class Context : DbContext
+    {
+        public Context() { }
+
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Direcciones> Direcciones { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}

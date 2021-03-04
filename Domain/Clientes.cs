20bb717f55b45175aaf283cfb66a802997b519f2
Lalo Flores <eflores@crudcreativo.com>
2021-03-04 16:58:14 -0400

    Add project files.

---
+++ Domain/Clientes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Clientes
+    {
+
+        public int Id { get; set; }
+        public string  Nombre { get; set; }
+        public string Telefono { get; set; }
+
+        public virtual Direcciones Direccion { get; set; }
+    }
+}
---
+++ Domain/Direcciones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Direcciones
+    {
+        public int Id { get; set; }
+        public string Direccion { get; set; }
+    }
+}
